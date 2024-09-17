f6a06c6dc3547dbc36a440f5e456e807c4cd4562
GageKolojaco <111771633+GageKolojaco@users.noreply.github.com>
2024-09-16 20:51:39 -0500

    added LoadDiagnostics()

--- Python/HW1/HW1.py
+++ Python/HW1/HW1.py
@@ -5,10 +5,33 @@ def Main():
     return power
     
 def LoadDiagnostics(filehandle):
-    #loop through the file and ...
+    #find the maximum length of the strings
+    max_length = max(len(s) for s in strings)
+    #create string for gamma rate
+    gamma_rate = ''
+    #create a list of lists for each index position
+    list_of_binary_str = [[] for _ in range(max_length)]
+    
+    #loop through the each string in the file
     for line in filehandle.readlines():
-        print(line)
-    filehandle.close() 
+        #loop through each character in each string
+        for index, char in enumerate(line):
+            #append the element to its respective list
+            list_of_binary_str[index].append(char)
+    filehandle.close()
+    
+    #loop through list of respective values
+    for list_of_indexes in list_of_binary_str:
+        for binary_str in list_of_indexes:
+            count_0 = string.count('0')
+            count_1 = string.count('1')
+            if count_0 > count_1
+                gamma_rate + 0
+            else
+                gamma_rate + 1
+    return gamma_rate
+
+            
 
 if __name__ == '__main__':
     Main()
